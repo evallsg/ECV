eec211ac91fbf1062a68e4f752b609274b037fc9
Unknown <mjuvilla@gmail.com>
2018-02-20 15:37:10 +0100

    Added more stuff to the server

--- Practica3/server/server.js
+++ Practica3/server/server.js
@@ -2,57 +2,99 @@ var http = require('http');
 var url = require('url');
 var fs = require('fs');
 
-var server = http.createServer(function(request, response) {
+function Book_Server(){
+    this.init();
+}
+
+// prototype for book objects
+function Book(book_name, book_author, state, chapters){
+    this.book_name = book_name;
+    this.book_author = book_author;
+    this.state = state;
+    this.chapters = chapters;
+}
+
+// prototype for book chapters
+// children should be a vector containing {id: next_chapter_idx, decision_text: "text of the decision leading to next_chapter_idx"}
+function Chapter(parent, chapter_author){
+        this.id = -1;
+        this.parent = parent;
+        this.chapter_author = chapter_author;
+        this.title = "";
+        this.text = "";
+        this.state = "on_progress";
+        this.is_terminal = 0;
+        this.children = [];
+}
+
+Book_Server.prototype.updateBookCollection = function()
+{
+    //TODO: save the book collection file
+}
+
+function processRequest(request, response)
+{
     console.log("REQUEST: " + request.url);
 
-    request_parsed = request.url.split("/").slice(1)
-    //console.log(request_parsed);
+        request_parsed = request.url.split("/").slice(1)
+        //console.log(request_parsed);
 
-    if (request_parsed[0] == "getbookchapter" && request_parsed.length == 3) {
-        var chapter = getChapterInfo(request_parsed[1], request_parsed[2]);
-        response.end(JSON.stringify(chapter));
-    } else if (request_parsed[0] == "setbookchapter" && request_parsed.length == 2) {
-        
-		request.on('data', (chunk) => {
-		  console.log(`Received ${chunk.length} bytes of data.`);
-		});
-		request.on('end', () => {
-		  console.log('There will be no more data.');
-		});
-        
-        var url_info = url.parse(request.url, true);
-        //console.log(url_info);
-        var book_name = url_info.pathname.split("/").slice(2);
-        //console.log(book_name);
-        var params = url_info.query;
-        console.log("Setting new chapter for " + book_name);
-        console.log(params);
-        // TODO: do stuff with it
+        if (request_parsed[0] == "getbookchapter" && request_parsed.length == 3) {
+            var chapter = this.getChapterInfo(request_parsed[1], request_parsed[2]);
+            response.end(JSON.stringify(chapter));
+        } else if (request_parsed[0] == "createbookchapter" && request_parsed.length == 2) {
+            // what we need: decision_text, book_name, parent chapter, chapter_author
+            var decision_text;
+            var book_name = request_parsed[1];
+            var parent_chapter_id;
+            var chapter_author;
 
-        response.end();
+            new_chapter = new Chapter(parent_chapter_id, chapter_author);
 
-        // setChapterInfo(params[1], params[2]);
-        // response.end(JSON.stringify(chapter));
-    }
+            this.createNewChapter(book_name, new_chapter, decision_text)
 
-    // var url_info = url.parse( request.url, true ); //all the request info is here
-    // //console.log(url_info);
-    // var pathname = url_info.pathname; //the address
-    // //console.log(pathname);
-    // var params = url_info.query; //the parameters
-    // //console.log(params)
-    // response.end("OK!"); //send a response
-});
+        } else if (request_parsed[0] == "savebookchapter" && request_parsed.length == 2) {
+            data = ""
+            var url_info = url.parse(request.url, true);
+            //console.log(url_info);
+            var book_name = url_info.pathname.split("/").slice(2);
 
-server.listen(13456, function() {
-    console.log("Server ready!");
-});
+            request.on('data', (chunk) => {
+                data += chunk
+              console.log(`Received ${chunk.length} bytes of data.`);
+            });
+            request.on('end', () => {
+              console.log('There will be no more data.');
+              console.log(data)
+            });
 
-function getChapterInfo(book_name, chapter_id) {
+            response.end();
+
+            // setChapterInfo(params[1], params[2]);
+            // response.end(JSON.stringify(chapter));
+        }
+
+}
+
+Book_Server.prototype.init = function()
+{
+    this.books_collection = JSON.parse(fs.readFileSync('books.json', 'utf8'));
+
+    //console.log(this.books_collection)
+
+    this.server = http.createServer(processRequest.bind(this));
+
+    this.server.listen(14546, function() {
+        console.log("Server ready!");
+    });
+
+}
+
+Book_Server.prototype.getChapterInfo = function(book_name, chapter_id) {
     console.log("Getting book " + book_name + " chapter " + chapter_id);
-    var books_collection = JSON.parse(fs.readFileSync('books.json', 'utf8'));
 
-    var book = books_collection.find(o => o.book_name === book_name);
+    var book = this.books_collection.find(o => o.book_name === book_name);
+    console.log(book);
 
     if (book == null)
         return "None";
@@ -63,17 +105,36 @@ function getChapterInfo(book_name, chapter_id) {
     return chapter;
 }
 
-function setChapterInfo(book_name, chapter_id) {
-    console.log("Getting book " + book_name + " chapter " + chapter_id);
-    var books_collection = JSON.parse(fs.readFileSync('books.json', 'utf8'));
+Book_Server.prototype.createNewChapter = function(book_name, chapter, decision_text) {
 
-    var book = books_collection.find(o => o.book_name === book_name);
+    var book = this.books_collection.find(o => o.book_name === book_name);
 
-    if (book == null)
-        return "None";
-    //console.log(chapters)
-    var chapter = book["chapters"].find(o => o.id == chapter_id);
-    if (chapter == null)
-        return "None";
-    return chapter;
+    chapter.id = book.chapters.length;
+
+    console.log("Creating new chapter in " + book_name + " with id " + chapter.id);
+
+    book.chapters += chapter;
+
+    console.log(book.chapters);
+
+    this.updateBookCollection();
 }
+
+Book_Server.prototype.saveBookChapter = function(book_name, chapter) {
+
+    //TODO: get chapter info
+    // update relevant chapter info (state, text, title, decisions...)
+
+    console.log("Getting book " + book_name + " chapter " + chapter_id);
+    var book = this.books_collection.find(o => o.book_name === book_name);
+
+    // if (book == null)
+    //     return "None";
+    // //console.log(chapters)
+    // var chapter = book["chapters"].find(o => o.id == chapter_id);
+    // if (chapter == null)
+    //     return "None";
+    // return chapter;
+}
+
+server = new Book_Server();
