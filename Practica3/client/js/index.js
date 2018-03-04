2241414890ff6ff3d4bb6fe667a07c3349858b3a
EvaValls <eva.valls01@estudiant.upf.edu>
2018-03-04 23:23:36 +0100

    add events to go to register view

--- Practica3/client/js/index.js
+++ Practica3/client/js/index.js
@@ -17,12 +17,20 @@ function onLoginClicked()
 	this.client.requestLogin(user, password, userTokenReceived)
 
 }
+function onRegisterClicked(){
+	console.log("enter")
+	document.getElementsByClassName("login-title")[0].innerText= "REGISTER";
+	document.getElementById("input-username").classList.remove("hidden");
+	document.getElementById("btn-login").classList.add("hidden");
+	document.getElementById("btn-register").classList.remove("hidden")
+	document.getElementsByClassName("login-btn-reg")[0].classList.add("hidden")
+}
 
 function init() {
 
 	//document.getElementsByTagName("body")[0].style.display = "initial";
 	document.getElementsByClassName("login-btn-lgn")[0].addEventListener("click", onLoginClicked.bind(this), false);
-   
+    document.getElementsByClassName("login-btn-reg")[0].addEventListener("click", onRegisterClicked.bind(this), false);
 }
 
 var user_token = localStorage.getItem("user-token")
